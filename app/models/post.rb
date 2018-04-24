4edad270ec4d54de435dd8472b55738d4f85eca9
Winston Kotzan <wak@wakproductions.com>
2018-04-23 23:55:10 -0400

    Fix ordering on video display

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   self.inheritance_column = :disabled
   belongs_to :video_metadata
 
-  scope :videos, -> { where(type: 'video').order(publish_date: :desc) }
+  # created_at's probably not the best secondary means of ordering, but is a good enough band aid for now
+  scope :videos, -> { where(type: 'video').order(publish_date: :desc, created_at: :desc) }
 
   def increment_view_count
     update(view_count: view_count + 1)
