e364c14c0a1e5bcac65462cb1843cb4de7799fda
Winston Kotzan <wak@wakproductions.com>
2018-05-06 23:29:17 -0400

    Dynamically load route for customization

--- Gemfile
+++ Gemfile
@@ -48,4 +48,4 @@ end
 group :development do
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
-endgem 'reflexivity-videos', path: 'reflexivity-videos'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
   resources :videos, only: [:index]
   get '/videos/:permalink_id', to: 'videos#show', as: 'video'
 
+  Dir.entries(File.join(Dir.pwd, 'engines-customization')).reject {|name| name =~ %r{\.{1,2}}} .each do |engine|
+    namespace = engine.camelcase
+    mount "#{namespace}::Engine".constantize => "#{namespace}::MOUNTED_PATH".constantize
+  end
+
+  # mount ReflexivityVideos::Engine => 'videos'
+
   root 'home#index'
 end
